718d2c472ef4c302be1df152b33b3adfb090ce05
fmoorenoo <fermontemore@gmail.com>
2024-12-04 08:52:35 +0000

    MarkerTypeDAO.kt creado con métodos para insertar, ver y obtener un tipo de marcador por su ID

---
+++ app/src/main/java/org/iesharia/maproomapp/MarkerTypeDAO.kt
@@ -0,0 +1,17 @@
+package org.iesharia.maproomapp
+
+import androidx.room.*
+
+@Dao interface MarkerTypeDao {
+    // Añadir tipo de marcador
+    @Insert
+    suspend fun insertMarkerType(markerType: MarkerType)
+
+    // Obtener todos los tipos de marcadores
+    @Query("SELECT * FROM marker_types")
+    suspend fun getAllMarkerTypes(): List<MarkerType>
+
+    // Obtener un tipo de marcador según su ID
+    @Query("SELECT * FROM marker_types WHERE id = :id")
+    fun getMarkTypeById(id: Int): MarkerType?
+}
