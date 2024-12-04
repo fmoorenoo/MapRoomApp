1dbddaca68cb29171cb4670c76716418e0a30da1
fmoorenoo <fermontemore@gmail.com>
2024-12-04 08:38:18 +0000

    Creado Marker.kt para la tabla 'markers' de la base de datos

---
+++ app/src/main/java/org/iesharia/maproomapp/Marker.kt
@@ -0,0 +1,28 @@
+package org.iesharia.maproomapp
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
+
+@Entity(
+    tableName = "markers",
+    // Fuente: https://stackoverflow.com/questions/47511750/how-to-use-foreign-key-in-room-persistence-library
+    foreignKeys = [
+        ForeignKey(
+            entity = MarkerType::class,
+            // Referencia en la tabla MarkerType
+            parentColumns = ["id"],
+            // Referencia en esta tabla
+            childColumns = ["markTypeId"],
+        )
+    ]
+)
+
+data class Marker(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    // Coordenadas geográficas
+    val location: String = "",
+    val MarkerTypeId: Int
+)
