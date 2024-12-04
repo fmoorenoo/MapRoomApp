f10d30dccd40f8b2c950c8ce1ed0aff990ddbe21
fmoorenoo <fermontemore@gmail.com>
2024-12-04 08:37:27 +0000

    Creado archivo MapDatabase.kt con código reciclado de 'TaskRoomApp'

---
+++ app/src/main/java/org/iesharia/maproomapp/MapDatabase.kt
@@ -0,0 +1,33 @@
+package org.iesharia.maproomapp
+
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Marker::class, MarkerType::class], version = 1)
+abstract class MapDatabase : RoomDatabase() {
+
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MapDatabase? = null
+
+        fun getDatabase(context: Context): MapDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MapDatabase::class.java,
+                    "map_database"
+                )
+                    // Si cambia la versión de la base de datos, esta se elimina y se recrea con las modificaciones.
+                    .fallbackToDestructiveMigration()
+                    // Esto hace que se pierdan los datos, pero es provisional mientras construyo la app
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
