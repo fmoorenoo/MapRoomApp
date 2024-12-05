b6bdbf94cc5ed3b63d1521a9f46caef3625695e6
fmoorenoo <fermontemore@gmail.com>
2024-12-05 13:08:42 +0000

    MainApp y LoadMap.kt creados

---
+++ app/src/main/java/org/iesharia/maproomapp/MainApp.kt
@@ -0,0 +1,26 @@
+package org.iesharia.maproomapp
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@Composable
+fun MainApp(database: MapDatabase) {
+
+}
