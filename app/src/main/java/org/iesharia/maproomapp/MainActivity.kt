04eb1884424fe4daf7c22cce66410fca7c4b573d
fmoorenoo <fermontemore@gmail.com>
2024-12-05 08:16:39 +0000

    Código reutilizado de otro proyecto para cargar el mapa

--- app/src/main/java/org/iesharia/maproomapp/MainActivity.kt
+++ app/src/main/java/org/iesharia/maproomapp/MainActivity.kt
@@ -1,18 +1,57 @@
 package org.iesharia.maproomapp
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.utsman.osmandcompose.DefaultMapProperties
+import com.utsman.osmandcompose.Marker
+import com.utsman.osmandcompose.OpenStreetMap
+import com.utsman.osmandcompose.ZoomButtonVisibility
+import com.utsman.osmandcompose.rememberCameraState
+import com.utsman.osmandcompose.rememberMarkerState
 import org.iesharia.maproomapp.ui.theme.MapRoomAppTheme
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
+import org.osmdroid.tileprovider.tilesource.XYTileSource
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.util.MapTileIndex
 
+
+val GoogleSat: OnlineTileSourceBase = object : XYTileSource(
+    "Google-Sat",
+    0, 19, 256, ".png", arrayOf<String>(
+        "https://mt0.google.com",
+        "https://mt1.google.com",
+        "https://mt2.google.com",
+        "https://mt3.google.com",
+
+        )
+) {
+    override fun getTileURLString(pTileIndex: Long): String {
+        return baseUrl + "/vt/lyrs=s&x=" + MapTileIndex.getX(pTileIndex) + "&y=" + MapTileIndex.getY(
+            pTileIndex
+        ) + "&z=" + MapTileIndex.getZoom(pTileIndex)
+    }
+}
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +59,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MapRoomAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MyMapView(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +69,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MapRoomAppTheme {
-        Greeting("Android")
+fun MyMapView(modifier: Modifier = Modifier) {
+    // define camera state
+    val cameraState = rememberCameraState {
+        geoPoint = GeoPoint(28.957473, -13.554514)
+        zoom = 17.0 // optional, default is 5.0
     }
+
+    // define properties with remember with default value
+    var mapProperties by remember {
+        mutableStateOf(DefaultMapProperties)
+    }
+
+    // setup mapProperties in side effect
+    SideEffect {
+        mapProperties = mapProperties
+            .copy(tileSources = GoogleSat)
+            .copy(isEnableRotationGesture = true)
+            .copy(zoomButtonVisibility = ZoomButtonVisibility.NEVER)
+    }
+
+    // define marker state
+    val depokMarkerState = rememberMarkerState(
+        geoPoint = GeoPoint(28.957473, -13.554514)
+    )
 }
