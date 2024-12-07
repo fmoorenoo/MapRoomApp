dd30eca9b0e7aeeab08fa94effb1aa205d40e7a6
fmoorenoo <fermontemore@gmail.com>
2024-12-07 17:38:55 +0000

    Código simplificado haciendo uso de un ViewModel

--- app/src/main/java/org/iesharia/maproomapp/MainActivity.kt
+++ app/src/main/java/org/iesharia/maproomapp/MainActivity.kt
@@ -3,40 +3,25 @@ package org.iesharia.maproomapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.mutableStateListOf
+import org.iesharia.maproomapp.viewmodel.MapViewModel
 import org.iesharia.maproomapp.model.MapDatabase
 import org.iesharia.maproomapp.view.MainApp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.iesharia.maproomapp.model.Marker
-import org.iesharia.maproomapp.model.MarkerType
 
 class MainActivity : ComponentActivity() {
-
-    private lateinit var database: MapDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Inicializar la base de datos
-        database = MapDatabase.getDatabase(this)
-
-        // DAO para los tipos de marcadores
+        val database = MapDatabase.getDatabase(this)
+        val markerDao = database.markerDao()
         val markerTypeDao = database.markerTypeDao()
 
-        // Lista mutable para almacenar los marcadores
-        val markers = mutableStateListOf<Marker>()
-
-        // Obtener los marcadores de la base de datos
-        CoroutineScope(Dispatchers.IO).launch {
-            val markerDao = database.markerDao()
-            val dbMarkers = markerDao.getAllMarkers()
-            markers.addAll(dbMarkers)
-        }
+        // Crear el ViewModel
+        val mapViewModel = MapViewModel(markerDao, markerTypeDao)
 
         setContent {
-            MainApp(database, markers, markerTypeDao)
+            MainApp(mapViewModel)
         }
     }
 }
