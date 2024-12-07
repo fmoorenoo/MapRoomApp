e917336ecb9bb8fd32d609beaac4f68e44b078d6
fmoorenoo <fermontemore@gmail.com>
2024-12-07 14:05:43 +0000

    Función provisional para añadir tipos de marcadores en MainActivity.kt

--- app/src/main/java/org/iesharia/maproomapp/MainActivity.kt
+++ app/src/main/java/org/iesharia/maproomapp/MainActivity.kt
@@ -5,20 +5,47 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import org.iesharia.maproomapp.model.MapDatabase
 import org.iesharia.maproomapp.view.MainApp
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.iesharia.maproomapp.model.MarkerType
 
 class MainActivity : ComponentActivity() {
 
-        private lateinit var database: MapDatabase
+    private lateinit var database: MapDatabase
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-            // Inicializar la base de datos
-            database = MapDatabase.getDatabase(this)
+        // Inicializar la base de datos
+        database = MapDatabase.getDatabase(this)
 
-            setContent {
-                MainApp(database)
-            }
+        // Insertar los tipos de marcadores en la base de datos
+        // insertMarkerTypes()
+
+        setContent {
+            MainApp(database)
+        }
     }
+
+
+    // Función para insertar tipos de marcadores
+    /*
+    private fun insertMarkerTypes() {
+        val markerTypeDao = database.markerTypeDao()
+        val markerTypes = listOf(
+            MarkerType(name = "Ocio"),
+            MarkerType(name = "Comida"),
+            MarkerType(name = "Monumento"),
+            MarkerType(name = "Otro")
+        )
+
+        // Insertar los tipos de marcadores en un hilo diferente al de la interfaz
+        CoroutineScope(Dispatchers.IO).launch {
+            markerTypes.forEach { markerType ->
+                markerTypeDao.insertMarkerType(markerType)
+            }
+        }
+    }
+    */
 }
