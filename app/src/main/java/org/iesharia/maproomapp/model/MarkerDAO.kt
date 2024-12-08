14d8c51dea18c5bdd7ba704fe5a27eb3524f70c4
fmoorenoo <fermontemore@gmail.com>
2024-12-08 13:19:31 +0000

    Métodos para borrar marcadores y obtener marcadores por su ID

--- app/src/main/java/org/iesharia/maproomapp/model/MarkerDAO.kt
+++ app/src/main/java/org/iesharia/maproomapp/model/MarkerDAO.kt
@@ -7,7 +7,15 @@ import androidx.room.*
     @Insert
     suspend fun insert(marker: Marker)
 
+    // Borrar marcadores
+    @Delete
+    suspend fun delete(marker: Marker?)
+
     // Obtener todos los marcadores
     @Query("SELECT * FROM markers")
     suspend fun getAllMarkers(): List<Marker>
+
+    // Obtener un marcador por su ID
+    @Query("SELECT * FROM markers WHERE id = :id")
+    fun getMarkerById(id: Int): Marker?
 }
